fdddc6da9358cac905c1dfdab3a7341b972ed184
Joe Winkler <joe.c.winkler@outlook.com>
2018-05-04 19:29:13 -0400

    Created route on /api/beers/:beer_id for DELETE

--- index.js
+++ index.js
@@ -94,6 +94,17 @@ beerRoute.put(function(req, res) {
   });
 });
 
+// Create endpoint on /api/beers/:beer_id for DELETE
+beerRoute.delete(function(req, res) {
+  // Use the Beer model to find a specific beer and remove it
+  Beer.findByIdAndRemove(req.params.beer_id, function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer removed from the locker!' });
+  });
+});
+
 // Register all routes with /api
 app.use('/api', router);
 
